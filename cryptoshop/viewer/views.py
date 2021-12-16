7d7d8e41da6865bd70fb6acccca16a962921fc17
Jakub <j.kornacki@wp.pl>
2021-12-16 19:04:09 +0100

    templates (base, index), views (IndexView), url (index)

--- cryptoshop/viewer/views.py
+++ cryptoshop/viewer/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import Article
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    model = Article
+    context_object_name = 'index'
+
 
 # Create your views here.
