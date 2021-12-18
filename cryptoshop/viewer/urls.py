7223d5a7dcc5d44b0eae7a2477f0f08035fd1373
Emil <emil.zarzecki@gmail.com>
2021-12-18 16:14:35 +0100

    create model adverts

--- cryptoshop/viewer/urls.py
+++ cryptoshop/viewer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, AdvertView
 
 urlpatterns = [
 
-    path('index', IndexView.as_view(), name='index')
-
+    path('index', IndexView.as_view(), name='index'),
+    path('adverts', AdvertView.as_view(), name='adverts')
 ]
--- cryptoshop/viewer/views.py
+++ cryptoshop/viewer/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
-from .models import Article
+from .models import Article, Adverts
 
 class IndexView(ListView):
     template_name = 'index.html'
     model = Article
     context_object_name = 'index'
 
+
+class AdvertView(ListView):
+    template_name = 'home.html'
+    model = Adverts
+    context_object_name = 'adverts'
 
 # Create your views here.
