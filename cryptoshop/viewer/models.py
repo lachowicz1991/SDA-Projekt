4e73b40f7d93a1a274d6b7b9c54caff550e041c2
Emil <emil.zarzecki@gmail.com>
2021-12-20 21:21:42 +0100

    change models.py (add FAQ and Adverts and Articles)

--- cryptoshop/viewer/models.py
+++ cryptoshop/viewer/models.py
@@ -1,7 +1,6 @@
-from django.db.models import Model, TextField, CharField, FloatField, ImageField
-
+from django.db.models import Model, TextField, CharField, FloatField, ImageField, SlugField
+from django.utils.text import slugify
 # Create your models here.
-
 
 #  Model used for the articles with external URLs for images.
 class Article(Model):
@@ -9,9 +8,10 @@ class Article(Model):
 	content = TextField()
 	image = ImageField(upload_to='articles', blank=True)
 
-
+# Model Adverts similar to article
 class Adverts(Model):
 	title = CharField(max_length=128)
+	content = TextField()
 	image = ImageField(upload_to='articles', blank=True)
 
 
@@ -29,8 +29,11 @@ class Product(Model):
 class FAQ(Model):
 	question = CharField(max_length=128)
 	answer = TextField()
-	# Jakub branch
+	slug = SlugField(max_length=128, unique=True)
 
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = slugify(self.question)
+		super().save(*args, **kwargs)
 
-# Matt branch
 
