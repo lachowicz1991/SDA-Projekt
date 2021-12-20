20f03c1c0e8735b87da659acff7a6db6491546e6
Emil <emil.zarzecki@gmail.com>
2021-12-20 21:15:45 +0100

    change urls (add FAQ and FAQ create, detail, delete)

--- cryptoshop/viewer/urls.py
+++ cryptoshop/viewer/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .views import IndexView, AdvertView
+from .views import IndexView, CoursesView, FaqView, FaqCreateView, FaqUpdateView, FaqDeleteView, FaqDetailView, AdvertView
 
 urlpatterns = [
 
-    path('index', IndexView.as_view(), name='index'),
-    path('adverts', AdvertView.as_view(), name='adverts')
+    path('', IndexView.as_view(), name='index'),
+    path('adverts', AdvertView.as_view(), name='adverts'),
+    path('courses', CoursesView.as_view(), name='courses'),
+    path('faq', FaqView.as_view(), name='faq'),
+    path('faq/new', FaqCreateView.as_view(), name='faq-create'),
+    path('faq/<slug:slug>', FaqDetailView.as_view(), name='faq-detail'),
+    path('faq/<slug:slug>/edit', FaqUpdateView.as_view(), name='faq-update'),
+    path('faq/<slug:slug>/delete', FaqDeleteView.as_view(), name='faq-delete'),
 ]
+
