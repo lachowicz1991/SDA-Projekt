1ca827df9786e33e5a0cd8986f4ad0f2eab9ddc4
Jakub <j.kornacki@wp.pl>
2021-12-22 20:35:14 +0100

    News App (view, html, url)

--- cryptoshop/viewer/urls.py
+++ cryptoshop/viewer/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+from . import views
+
 from .views import IndexView
 
 urlpatterns = [
 
-    path('index', IndexView.as_view(), name='index')
+    path('index', IndexView.as_view(), name='index'),
+    path('news', views.home, name='home')
 
 ]
--- cryptoshop/viewer/views.py
+++ cryptoshop/viewer/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
+import requests
 from django.views.generic import TemplateView, ListView
 from .models import Article
+
+API_KEY = 'ff230ce135704fccb7a61b36132c35f9'
+
 
 class IndexView(ListView):
     template_name = 'index.html'
     model = Article
     context_object_name = 'index'
 
+
+def home(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        print(articles)
+
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        print(articles)
+
+    context = {
+        'articles' :articles
+    }
+
+    return render(request, 'news.html', context)
 
 # Create your views here.
