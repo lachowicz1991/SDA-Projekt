b2dd5c408ef13fcff596b6ddc39eee2b2425f0b8
lachowicz1991 <lachowicz1991@protonmail.com>
2021-12-26 11:37:14 +0100

    Detail View added for courses.

--- cryptoshop/cart/urls.py
+++ cryptoshop/cart/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import cart, checkout, updateItem, processOrder, store
+from .views import cart, checkout, updateItem, processOrder, store, CourseDetailView
 
 urlpatterns = [
     path('store/', store, name='store'),
 	path('cart/', cart, name='cart'),
 	path('checkout/', checkout, name='checkout'),
 	path('update_item/', updateItem, name='update_item'),
-	path('process_order/', processOrder, name='process_order')
+	path('process_order/', processOrder, name='process_order'),
+	path('product_detail/<int:pk>', CourseDetailView.as_view(), name='product_detail')
 ]
