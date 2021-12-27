39dcef9d9e59a9b921ea00ad83a5c48538d7faf2
lachowicz1991 <lachowicz1991@protonmail.com>
2021-12-27 21:08:35 +0100

    Update/Delete/Create View configured for courses. (Image upload support fixed /media/images/)

---
+++ cryptoshop/cart/forms.py
@@ -0,0 +1,28 @@
+from django.forms import CharField, DateField, BooleanField, CheckboxInput, Textarea, ImageField, Form, FloatField, IntegerField, ModelChoiceField, ModelForm, Textarea
+from django.core.exceptions import ValidationError
+from .models import Product
+from datetime import date
+import re
+
+PREMIUM_CHOICE = (('Yes', 'Yes'), ('No', 'No'))
+
+
+class ProductForm(ModelForm):
+	class Meta:
+		model = Product
+		fields = '__all__'
+
+	PREMIUM_CHOICE = (('Yes', 'Yes'), ('No', 'No'))
+
+	# name = CharField(max_length=200)
+	# short_description = CharField(max_length=200, widget=Textarea)
+	# price = FloatField()
+	# digital = BooleanField(widget=CheckboxInput)
+	# image = ImageField()
+	# premium = CharField(max_length=100, choices=PREMIUM_CHOICE)
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for visible in self.visible_fields():
+			visible.field.widget.attrs['class'] = 'form-control'
+
--- cryptoshop/cart/urls.py
+++ cryptoshop/cart/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import cart, checkout, updateItem, processOrder, store, CourseDetailView
+from .views import cart, checkout, updateItem, processOrder, store, CourseDetailView, ProductCreateView,\
+	ProductDeleteView, ProductUpdateView
 
 urlpatterns = [
-    path('store/', store, name='store'),
+	path('store/', store, name='store'),
 	path('cart/', cart, name='cart'),
 	path('checkout/', checkout, name='checkout'),
 	path('update_item/', updateItem, name='update_item'),
 	path('process_order/', processOrder, name='process_order'),
-	path('product_detail/<int:pk>', CourseDetailView.as_view(), name='product_detail')
+	path('product_detail/<int:pk>', CourseDetailView.as_view(), name='product_detail'),
+	path('product_detail/<int:pk>/delete', ProductDeleteView.as_view(), name='product-delete'),
+	path('product_detail/<int:pk>/update', ProductUpdateView.as_view(), name='product-update'),
+	path('store/create', ProductCreateView.as_view(), name='product-create'),
 ]
