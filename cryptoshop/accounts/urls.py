e51804bde356c7b8ea14a13cf3a449e300a0284b
lachowicz1991 <lachowicz1991@protonmail.com>
2022-01-04 22:00:31 +0100

    Working User registration with Customer (2 groups required in database for  current functionality - customers and admin(adminpanel) *work in progress*

--- cryptoshop/accounts/urls.py
+++ cryptoshop/accounts/urls.py
@@ -1,15 +1,13 @@
-from .views import CustomLoginView, CustomPasswordChangeView, Controls
+from .views import CustomLoginView, CustomPasswordChangeView, Controls, customer_registration
 from django.contrib.auth.views import LogoutView, PasswordChangeView
-#from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
-	path('login', CustomLoginView.as_view(), name='login'),
+	path('login/', CustomLoginView.as_view(), name='login'),
 	path('logout', LogoutView.as_view(), name='logout'),
 	path('change_password', CustomPasswordChangeView.as_view(), name='change_password'),
 	path('controls', Controls.as_view(), name='controls'),
-    path('', include('viewer.urls'))
-
+	path('signup', customer_registration, name='signup'),
+	path('', include('viewer.urls'))
 ]
--- cryptoshop/accounts/views.py
+++ cryptoshop/accounts/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, reverse_lazy, LogoutView, PasswordChangeView
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, CreateView
+from .forms import UserCreationForm, CreateUserForm
 from .models import Customer
+from django.contrib import messages
+from django.contrib.auth.models import Group
+from django.contrib import messages
+from .decorators import unauthenticated_user
 # Create your views here.
 
+
+def customer_registration(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+            )
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('index')
+    context = {'form': form}
+    return render(request, 'signup.html', context)
+
+def account_view(request):
+    orders = req
+    context = {}
+    return render(request, 'account.html', context)
 
 class CustomLoginView(LoginView):
     template_name = 'forms.html'
