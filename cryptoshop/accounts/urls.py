22eda8b8ee6ca1870c8e37f4ce4fad7463d06933
lachowicz1991 <lachowicz1991@protonmail.com>
2022-01-05 18:42:10 +0100

    Working User managment with filtring by group

--- cryptoshop/accounts/urls.py
+++ cryptoshop/accounts/urls.py
@@ -1,4 +1,4 @@
-from .views import CustomLoginView, CustomPasswordChangeView, Controls, customer_registration
+from .views import CustomLoginView, CustomPasswordChangeView, Controls, UserDeleteView, customer_registration,staff_list_view, customer_list_view
 from django.contrib.auth.views import LogoutView, PasswordChangeView
 # from django.contrib import admin
 from django.urls import path, include
@@ -9,5 +9,8 @@ urlpatterns = [
 	path('change_password', CustomPasswordChangeView.as_view(), name='change_password'),
 	path('controls', Controls.as_view(), name='controls'),
 	path('signup', customer_registration, name='signup'),
+	path('customercontrol', customer_list_view, name='customercontrol'),
+	path('staffcontrol', staff_list_view, name='staffcontrol'),
+	path('<int:pk>/delete', UserDeleteView.as_view(), name='user-delete'),
 	path('', include('viewer.urls'))
 ]
--- cryptoshop/accounts/views.py
+++ cryptoshop/accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, reverse_lazy, LogoutView, PasswordChangeView
-from django.views.generic import ListView, TemplateView, CreateView
-from .forms import UserCreationForm, CreateUserForm
+from django.views.generic import ListView, TemplateView, CreateView, DeleteView
+from .forms import UserCreationForm, CreateUserForm, User
 from .models import Customer
 from django.contrib import messages
 from django.contrib.auth.models import Group
 from django.contrib import messages
+from django.db.models import Q
 from .decorators import unauthenticated_user
 # Create your views here.
 
@@ -29,9 +30,21 @@ def customer_registration(request):
     return render(request, 'signup.html', context)
 
 def account_view(request):
-    orders = req
+
     context = {}
     return render(request, 'account.html', context)
+
+
+def customer_list_view(request):
+    customer = Customer.objects.filter(user__groups__name='customer')
+    premium = Customer.objects.filter(user__groups__name='premium')
+    context = {'customer': customer, 'premium': premium}
+    return render(request, 'customercontrol.html', context)
+
+def staff_list_view(request):
+    staff = Customer.objects.filter(user__groups__name='staff')
+    context = {'staff': staff}
+    return render(request, 'staffcontrol.html', context)
 
 class CustomLoginView(LoginView):
     template_name = 'forms.html'
@@ -40,11 +53,10 @@ class CustomPasswordChangeView(PasswordChangeView):
     template_name = 'forms.html'
     success_url = reverse_lazy('index')
 
-
-class CustomerListView(ListView):
-    template_name = 'gear.html'
-    model = Customer
-    context_object_name = 'gear'
-
 class Controls(TemplateView):
     template_name = 'controls.html'
+
+class UserDeleteView(DeleteView):
+    template_name = 'delete_form.html'
+    model = Customer
+    success_url = reverse_lazy('controls')
