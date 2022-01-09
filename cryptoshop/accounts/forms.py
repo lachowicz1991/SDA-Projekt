d822b056aec47aa6b6691ba17d1b77fbf4158f2e
lachowicz1991 <lachowicz1991@protonmail.com>
2022-01-09 16:25:41 +0100

    Working user registration with customer creation and dob/mobile

--- cryptoshop/accounts/forms.py
+++ cryptoshop/accounts/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django.forms import ModelForm, DateField, CharField
 from .models import Customer
 from django import forms
 
@@ -10,3 +10,12 @@ class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+	mobile = CharField(max_length=30)
+
+class CreateCustomerForm(ModelForm):
+	class Meta:
+		model = Customer
+		fields = ['mobile', 'dob']
+
+
+
