877bf9bbbe38cd4760933a7a0e666b1f634f9f06
Emil <emil.zarzecki@gmail.com>
2022-01-10 20:03:13 +0100

    wersja beta ze zmianami w advert.html, advert_form.html, models.py, settings.py

--- cryptoshop/viewer/models.py
+++ cryptoshop/viewer/models.py
@@ -11,7 +11,7 @@ class Article(Model):
 class Advert(Model):
 	title = CharField(max_length=128)
 	content = TextField()
-	image = ImageField(upload_to='adverts', blank=True)
+	image = ImageField(upload_to='images', blank=True, null=True)
 	slug = SlugField(max_length=128, unique=True)
 
 	def save(self, *args, **kwargs):
@@ -19,9 +19,17 @@ class Advert(Model):
 			self.slug = slugify(self.title)
 		super().save(*args, **kwargs)
 
-#	def __str__(self):
-#		return self.title
+	def __str__(self):
+		return self.title
 
+	@property
+	def image_url_safe(self):
+		try:
+			url = self.image.url
+
+		except:
+			url = ''
+		return url
 
 class Product(Model):
 	PREMIUM_CHOICE = (('Yes', 'Yes'), ('No', 'No'))
