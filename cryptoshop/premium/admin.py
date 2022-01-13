3ce45c4d67fe0a74549f7f4aaf62c02633f4a5c0
Koszulkowski <tomekgorzula@gmail.com>
2022-01-13 17:04:25 +0100

    Add Models, Views and templates in section Premium

--- cryptoshop/premium/admin.py
+++ cryptoshop/premium/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Predictions, InvestmentStrategies, BreakingNews
 # Register your models here.
+
+admin.site.register(BreakingNews)
+admin.site.register(Predictions)
+admin.site.register(InvestmentStrategies)
---
+++ cryptoshop/premium/templates/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Advert, Article, FAQ
+# Register your models here.
+admin.site.register(Advert)
+admin.site.register(Article)
+admin.site.register(FAQ)
